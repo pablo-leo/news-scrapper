99a23f959feebd463688af26c75d602a9d46ddc7
pablo-leo <pablo.leo@alu.uclm.es>
2022-01-16 10:56:19 +0100

    Adding MarketWatch scrapper

---
+++ newspapers/marketwatch.py
@@ -0,0 +1,43 @@
+import re
+import pandas as pd
+from scrapper import Scrapper
+
+
+class MarketWatchScrapper(Scrapper):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @staticmethod
+    def extract_title(soup):
+        title = soup.find('h1', class_='article__headline').text
+        title = title.replace('\n', '').strip()
+        return title
+
+    @staticmethod
+    def extract_subtitle(soup):
+        subtitle = soup.find('h2', class_='article__subhead').text
+        subtitle = subtitle.replace('\n', '').strip()
+        return subtitle
+
+    @staticmethod
+    def extract_author(soup):
+        author = soup.find('div', class_='byline article__byline').a.text
+        return author
+
+    @staticmethod
+    def extract_date(soup):
+        date = soup.find('time', class_='timestamp timestamp--pub').text
+        date = date.replace('\n', '').strip()
+        date = date.split('Published: ')[-1]
+        date = pd.to_datetime(date)
+        return date
+
+    @staticmethod
+    def extract_body(soup):
+        body_paragraphs = soup.find('div', class_='column column--full article__content').find_all('p')
+        body = []
+        for body_p in body_paragraphs:
+            body_p_text = body_p.text.replace('\n', ' ')
+            body.append(re.sub(' +', ' ', body_p_text))
+        body = '\n'.join(body)
+        return body
---
+++ tests/test_marketwatch.py
@@ -0,0 +1,13 @@
+from newspapers.marketwatch import MarketWatchScrapper
+
+urls = [
+    'https://www.marketwatch.com/story/why-new-years-chaos-may-signal-a-more-balanced-but-volatile-stock-market-in-2022-as-investors-grapple-with-a-hawkish-fed-11641653191?mod=home-page',
+    'https://www.marketwatch.com/story/3-key-takeaways-from-the-supreme-court-showdown-over-the-vaccine-or-test-rule-for-businesses-11641591945?mod=home-page',
+    'https://www.marketwatch.com/story/2022-could-be-the-year-of-financial-reckoning-bankruptcy-cases-fell-dramatically-in-2021-but-new-challenges-await-11641296556?mod=home-page'
+]
+
+sc = MarketWatchScrapper(urls=urls, verbose=True)
+sc.download_htmls()
+sc.extract_info()
+
+print('Holly smokes!')
